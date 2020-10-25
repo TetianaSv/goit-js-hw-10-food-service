4685408b6d0d57214807c7b0a2f72a2c2cc222c8
Tetiana Svinar <tiannadream@gmail.com>
2020-10-25 10:37:19 +0200

    hw10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './styles.css';
+import menuItemTpl from './templates/menu-item.hbs';
+import menu from "./menu.json"
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
+
+
+refs.menuList.innerHTML = menu.map(menuItemTpl).join('');
+if (JSON.parse(localStorage.getItem('checked-dark-theme'))) {
+  refs.body.classList.remove(`${Theme.LIGHT}`);
+  refs.body.classList.add(`${Theme.DARK}`);
+  refs.checkbox.setAttribute('checked', true);
+}
+
+refs.body.classList.add(`${Theme.LIGHT}`);
+
+refs.checkbox.addEventListener('change', checkboxChange);
+
+refs.body.classList.add(`${Theme.LIGHT}`);
+
+refs.checkbox.addEventListener('change', checkboxChange);
+
+function checkboxChange() {
+  refs.body.classList.toggle(`${Theme.LIGHT}`);
+  refs.body.classList.toggle(`${Theme.DARK}`);
+
+  if (refs.checkbox.getAttribute('checked') === 'true') {
+    refs.checkbox.removeAttribute('checked');
+    refs.checkbox.setAttribute('checked', false);
+
+    localStorage.removeItem('checked-dark-theme');
+    localStorage.setItem('checked-dark-theme', false);
+  } else {
+    refs.checkbox.removeAttribute('checked');
+    refs.checkbox.setAttribute('checked', true);
+
+    localStorage.removeItem('checked-dark-theme');
+    localStorage.setItem('checked-dark-theme', true);
+  }
+}
